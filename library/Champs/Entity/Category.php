baf24c30897d460954c26fd328114ceb21f8b79a
Ryan Chan <kimchan1314@gmail.com>
2012-11-07 21:51:01 +0800

    Updated Entity\Category.php and Entity\Newsfeed.php Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

--- library/Champs/Entity/Category.php
+++ library/Champs/Entity/Category.php
@@ -1,7 +1,92 @@
 <?php
 namespace Champs\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 
+/**
+ * @Entity
+ * @Table(name="categories")
+ * @HasLifecycleCallbacks
+ */
 class Category{
-
+    /**
+     *
+     * @var bigint $id
+     * @Column(type="bigint")
+     * @Id @GeneratedValue
+     */
+    private $id;
+    /**
+     *
+     * @var ArrayCollection $categories
+     * @ManyToOne(targetEntity="Category")
+     * @JoinColumn(name="parent_category_id", referencedColumnName="id")
+     */
+    private $categories;
+    /**
+     *
+     * @var string $category_name
+     * @Column(type="string")
+     */
+    private $category_name;
+    /**
+     *
+     * @var boolean $enabled
+     * @Column(type="boolean")
+     */
+    private $enabled;
+    /**
+     *
+     * @var smallint $order
+     * @Column(type="smallint")
+     */
+    private $order;
+    /**
+     *
+     * @var datetime $ts_created
+     * @Column(type="datetime")
+     */
+    private $ts_created;
+    /**
+     *
+     * @var datetime $ts_last_updated
+     * @Column(type="datetime", nullable=true)
+     */
+    private $ts_last_updated;
+    /**
+     * Initialize Method
+     */
+    public function __construct(){
+        $this->categories = new ArrayCollection();
+        $this->order = 0;
+        $this->enabled = true;
+    }
+    /**
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value){
+        $this->$key = $value;
+    }
+    /**
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key){
+        return $this->$key;
+    }
+    /**
+     * @PrePersist
+     */
+    public function doPrePersist(){
+        $this->ts_created = new \DateTime();
+    }
+    /**
+     * @PreUpdate
+     */
+    public function doPreUpdate(){
+        $this->ts_last_updated = new \DateTime();
+    }
 }
--- library/Champs/Entity/Newsfeed.php
+++ library/Champs/Entity/Newsfeed.php
@@ -1,16 +1,124 @@
 <?php
 namespace Champs\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 /**
  * @Entity
  * @Table(name="newsfeeds")
  * @HasLifecycleCallbacks
  */
 class Newsfeed{
+    /**
+     *
+     * @var bigint $id
+     * @Column(type="bigint")
+     * @Id @GeneratedValue
+     */
     private $id;
+    /**
+     *
+     * @var Champs\Entity\Category
+     * @ManyToOne(targetEntity="Category")
+     */
+    private $category;
+    /**
+     *
+     * @var Champs\Entity\User $user
+     * @ManToOne(targetEntity="User", inversedBy="newsfeeds")
+     */
     private $user;
+    /**
+     *
+     * @var text $content
+     * @Column(type="text")
+     */
     private $content;
+    /**
+     *
+     * @var datetime $ts_created
+     * @Column(type="datetime")
+     */
     private $ts_created;
+    /**
+     *
+     * @var datetime $ts_last_updated
+     * @Column(type="datetime", nullable=true)
+     */
     private $ts_last_updated;
+    /**
+     *
+     * @var ArrayCollection $profiles
+     * @OneToMany(targetEntity="NewsfeedProfile", mappedBy="newsfeed")
+     */
     private $profiles;
+    /**
+     *
+     * @var ArrayCollection $comments
+     * @ManyToOne(targetEntity="Newsfeed")
+     * @JoinColumn(name="parent_newsfeed_id", referencedColumnName="id")
+     */
+    private $newsfeeds;
+    /**
+     * Initialize Method
+     */
+    public function __construct(){
+        $this->profiles = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+    /**
+     *
+     * @param string $profile_key
+     * @param string $profile_value
+     */
+    public function setProfile(UserProfile $profile){
+        foreach ($this->profiles as $p){
+            if ($p->profile_key == $profile->profile_key){
+                $p->profile_value = $profile->profile_value;
+                return;
+            }
+        }
+
+        $this->profiles->add($profile);
+    }
+    /**
+     *
+     * @param string $key
+     */
+    public function unsetProfile($key){
+        foreach ($this->profiles as $profile){
+            if ($profile->profile_key == $key){
+                $this->profiles->removeElement($profile);
+            }
+        }
+    }
+    /**
+     *
+     * @param UserProfile $profile
+     */
+    public function getProfile($key = null){
+        if (strlen($key) > 0){
+            foreach ($this->profiles as $profile){
+                if ($profile->profile_key == $key){
+                    return $profile->profile_value;
+                }
+            }
+            return null;
+        } else {
+            return $this->profiles;
+        }
+    }
+    /**
+     *
+     * @return \DateTime
+     */
+    public function getCreated(){
+        return $this->ts_created->format('Y-m-d H:i:s');
+    }
+    /**
+     *
+     * @return \DateTime
+     */
+    public function getLastUpdated(){
+        return ($this->ts_last_updated == null) ? null : $this->ts_last_updated->format('Y-m-d H:i:s');
+    }
 }
