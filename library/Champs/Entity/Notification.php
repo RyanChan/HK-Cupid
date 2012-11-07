5b677a0a564684b7aa3fac1b273b3c83b43c5c38
Ryan Chan <kimchan1314@gmail.com>
2012-11-07 22:13:20 +0800

    Added Entity/Notification.php Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

---
+++ library/Champs/Entity/Notification.php
@@ -0,0 +1,89 @@
+<?php
+namespace Champs\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * @Entity
+ * @Table(name="notifications")
+ * @HasLifecycleCallbacks
+ */
+class Notification{
+    /**
+     *
+     * @var bigint $id
+     * @Column(type="bigint")
+     * @Id @GeneratedValue
+     */
+    private $id;
+    /**
+     *
+     * @var Champs\Entity\User $sender
+     * @OneToOne(targetEntity="User")
+     */
+    private $sender;
+    /**
+     *
+     * @var ArrayCollection $receivers
+     * @ManyToOne(targetEntity="User")
+     */
+    private $receivers;
+    /**
+     *
+     * @var text $message
+     * @Column(type="text")
+     */
+    private $message;
+    /**
+     *
+     * @var ArrayCollection $readReceivers
+     * @ManyToOne(targetEntity="User")
+     */
+    private $readReceivers;
+    /**
+     *
+     * @var string $targetType
+     * @Column(type="string")
+     */
+    private $targetType;
+    /**
+     *
+     * @var bigint $targetEntity
+     * @Column(type="bigint")
+     */
+    private $targetEntity;
+    /**
+     *
+     * @var datetime $ts_created
+     * @Column(type="datetime")
+     */
+    private $ts_created;
+    /**
+     * Initialize Method
+     */
+    public function __construct(){
+        $this->sender = new ArrayCollection();
+        $this->receivers = new ArrayCollection();
+        $this->readReceivers = new ArrayCollection();
+    }
+    /**
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value){
+        $this->$key = $value;
+    }
+    /**
+     *
+     * @param string $key
+     */
+    public function __get($key){
+        return $this->$key;
+    }
+    /**
+     * @PrePersist
+     */
+    public function doPrePersist(){
+        $this->ts_created = new ArrayCollection();
+    }
+}
