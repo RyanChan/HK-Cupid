a497f9d54b9d21fa60ddb7571e1f2a94895d4ef1
Ryan Chan <kimchan1314@gmail.com>
2012-11-10 11:12:41 +0800

    Updated

--- application/modules/dating/controllers/IndexController.php
+++ application/modules/dating/controllers/IndexController.php
@@ -6,6 +6,6 @@
  */
 class Dating_IndexController extends Zend_Controller_Action{
     public function indexAction(){
-        $this->view->hello = 'Hello';
+        $this->view->hello = 'Yo';
     }
 }
--- application/tmp/smarty_compile/0c9abab19070b99e3e5e96b3cd818421a4e7be48.file.layout.tpl.php
+++ application/tmp/smarty_compile/0c9abab19070b99e3e5e96b3cd818421a4e7be48.file.layout.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.12, created on 2012-11-09 10:56:11
+<?php /* Smarty version Smarty-3.1.12, created on 2012-11-09 11:50:39
          compiled from "/Applications/MAMP/htdocs/Champs/application/modules/dating/views/templates/layout.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:1503177962509c70cbcdb671-33235586%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0c9abab19070b99e3e5e96b3cd818421a4e7be48' => 
     array (
       0 => '/Applications/MAMP/htdocs/Champs/application/modules/dating/views/templates/layout.tpl',
-      1 => 1352429741,
+      1 => 1352433038,
       2 => 'file',
     ),
   ),
@@ -15,13 +15,13 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.12',
+  'unifunc' => 'content_509c70cbce1864_84769714',
   'variables' => 
   array (
     'this' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.12',
-  'unifunc' => 'content_509c70cbce1864_84769714',
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_509c70cbce1864_84769714')) {function content_509c70cbce1864_84769714($_smarty_tpl) {?><?php echo $_smarty_tpl->tpl_vars['this']->value->layout()->content;?>
-<?php }} ?>
+asd<?php }} ?>
