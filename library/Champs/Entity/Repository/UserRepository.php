8de872cb772048c778e6752cde6d663d581c5c74
Ryan Chan <kimchan1314@gmail.com>
2012-11-11 10:32:52 +0800

    Added functions to Champs\Entity\Repository\UserRepository.php

--- library/Champs/Entity/Repository/UserRepository.php
+++ library/Champs/Entity/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 namespace Champs\Entity\Repository;
 
 use Champs\Entity\User;
+use Champs\Entity\Role;
 use Doctrine\ORM\EntityRepository;
 
 /**
@@ -10,4 +11,93 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository{
 
+    /**
+     * Get a user object by email
+     *
+     * @param string $email
+     * @return Champs\Entity\User
+     */
+    public function getUserByEmail($email){
+        $em = $this->getEntityManager();
+
+        $query = $em->createQuery("SELECT u FROM Champs\Entity\User u, Champs\Entity\UserProfile up WHERE up.user = u and up.profile_key = 'email' and up.profile_value = ?1");
+
+        $query->setParameter(1, $email);
+
+        $result = $query->getSingleResult();
+
+        return $result ? $result : null;
+    }
+
+    /**
+     * Return true if the username doesn exist.
+     *
+     * @param string $username
+     * @return boolean
+     */
+    public function hasSameUsername($username){
+        $em = $this->getEntityManager();
+
+        $query = $em->createQuery("SELECT count(u.id) FROM Champs\Entity\User u WHERE u.username = ?1");
+
+        $query->setParameter(1, $username);
+
+        $count = $query->getSingleScalarResult();
+
+        return $count > 0;
+    }
+
+    /**
+     * Return true if the same email have found
+     *
+     * @param string $email
+     * @return boolean
+     */
+    public function hasSameEmail($email){
+        $em = $this->getEntityManager();
+
+        $query = $em->createQuery("SELECT count(u) FROM Champs\Entity\User u, Champs\Entity\UserProfile up WHERE up.user = u and up.profile_key = 'email' and up.profile_value = ?1");
+
+        $query->setParameter(1, $email);
+
+        $count = $query->getSingleScalarResult();
+
+        return $count > 0;
+    }
+
+    /**
+     * Return true if the same identity have found
+     *
+     * @param string $identity
+     * @return boolean
+     */
+    public function hasSameIdentity($identity){
+        $em = $this->getEntityManager();
+
+        $query = $em->createQuery("SELECT count(u) FROM Champs\Entity\User u, Champs\Entity\UserProfile up WHERE up.user = u and up.profile_key = 'identity' and up.profile_value = ?1");
+
+        $query->setParameter(1, $identity);
+
+        $count = $query->getSingleScalarResult();
+
+        return $count > 0;
+    }
+
+    /**
+     * Set role to user
+     *
+     * @param type $id
+     * @param Role $role
+     */
+    public function setUserToRole($id, Role $role){
+        $em = $this->getEntityManager();
+
+        $user = $this->find($id);
+
+        $user->role = $role;
+
+        $em->flush();
+
+        return $user;
+    }
 }
