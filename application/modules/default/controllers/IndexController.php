63d39d4b175b927358c52303052f2b7a6af2cd88
Ryan Chan <kimchan1314@gmail.com>
2012-11-13 10:23:49 +0800

    Updated application/modules/default/controllers/IndexController.php
    Modified parent class to MasterController

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,12 +1,20 @@
 <?php
+
+use Champs\Controller\MasterController;
+
 /**
  * Description of IndexController
  *
  * @author RyanChan
  */
-class IndexController extends Zend_Controller_Action{
-    public function indexAction(){
-        $em = Zend_Registry::get('doctrine')->getEntityManager();
-        $this->view->user = $em->getRepository('Champs\Entity\User')->isValidUsername('kimchan1314');
+class IndexController extends MasterController {
+
+    public function init(){
+        parent::init();
     }
+
+    public function indexAction() {
+        $this->view->defaultLocale = Zend_Registry::get('translate')->getList();
+    }
+
 }
