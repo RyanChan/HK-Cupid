05b07920af68f12a925b4043ae6f4f4e8fae7db4
Ryan Chan <kimchan1314@gmail.com>
2012-11-21 09:07:56 +0800

    Added account controller

---
+++ application/modules/default/controllers/AccountController.php
@@ -0,0 +1,13 @@
+<?php
+
+use Champs\Controller\MasterController;
+
+class AccountController extends MasterController{
+    public function init(){
+        parent::init();
+    }
+
+    public function indexAction(){
+        
+    }
+}
---
+++ application/modules/default/views/templates/account/index.tpl
@@ -0,0 +1,9 @@
+{include file="header.tpl" title="Account"}
+
+<div class="container clearfix">
+
+    
+
+</div>
+
+{include file="footer.tpl"}
