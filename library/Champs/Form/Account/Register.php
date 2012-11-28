888c44a9ea3dbcc537ca98c22f5a60427090dd15
Ryan Chan <kimchan1314@gmail.com>
2012-11-29 07:06:19 +0800

    Updated

--- library/Champs/Form/Account/Register.php
+++ library/Champs/Form/Account/Register.php
@@ -35,42 +35,39 @@ class Champs_Form_Account_Register extends Champs_FormProcessor {
             $this->addError('email', 'Please enter your e-mail address');
         else if (!$validator->isValid($this->email))
             $this->addError('email', 'Please enter a valid e-mail address');
-        else
+        else {
             $this->user->setUsername($this->email);
+            $this->user->setProfileWithKeyAndValue('email', $this->email);
+        }
 
         // first name
         $this->first_name = trim($request->getPost('first_name'));
 
         if (strlen($this->first_name) == 0)
             $this->addError('first_name', 'Please enter your first name');
+        else
+            $this->user->setProfileWithKeyAndValue('first_name', $this->first_name);
 
         // last name
         $this->last_name = trim($request->getPost('last_name'));
 
         if (strlen($this->last_name) == 0)
             $this->addError('last_name', 'Please enter your last name');
+        else
+            $this->user->setProfileWithKeyAndValue('last_name', $this->last_name);
 
         $this->password = trim($request->getPost('password'));
 
         if (strlen($this->password) < 8)
             $this->addError('password', 'Please enter a password that is longer than 8 characters');
         else
-        // set password
             $this->user->setPassword($this->password);
 
         if (!$this->hasError()) {
             try {
-                $user = $this->_userRepository->storeUserEntity($this->user);
-                // set first name profile
-                $user->setProfileWithKeyAndValue('first_name', $this->first_name);
-                // set last name profile
-                $user->setProfileWithKeyAndValue('last_name', $this->last_name);
-                // set email profile
-                $user->setProfileWithKeyAndValue('email', $this->email);
-                // update the profile
-                $this->em->flush();
+                $this->_userRepository->storeUserEntity($this->user);
 
-                $user->sendComfirmEmail();
+//                $user->sendComfirmEmail();
             } catch (Exception $e) {
                 $this->addError('error', $e->getMessage());
             }
