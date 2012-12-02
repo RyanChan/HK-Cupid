219c469f3cdf1399231ce07a61668d981f1fc645
Ryan Chan <kimchan1314@gmail.com>
2012-12-02 09:13:48 +0800

    Updated login action
    Added logout action

--- application/modules/default/controllers/AccountController.php
+++ application/modules/default/controllers/AccountController.php
@@ -1,10 +1,11 @@
 <?php
 
-class AccountController extends Champs_Controller_MasterController{
+class AccountController extends Champs_Controller_MasterController {
+
     /**
      * Initialize method
      */
-    public function init(){
+    public function init() {
         parent::init();
     }
 
@@ -13,8 +14,19 @@ class AccountController extends Champs_Controller_MasterController{
      *
      * show up all the information of the user
      */
-    public function indexAction(){
+    public function indexAction() {
 
+    }
+
+    public function logoutAction() {
+        $auth = Zend_Auth::getInstance();
+
+        if ($auth->hasIdentity()) {
+            $auth->clearIdentity();
+            $this->_redirect($this->getUrl('login'));
+        } else {
+            $this->_redirect($this->getUrl());
+        }
     }
 
     /**
@@ -22,10 +34,10 @@ class AccountController extends Champs_Controller_MasterController{
      *
      * show up the login page
      */
-    public function loginAction(){
+    public function loginAction() {
         $auth = Zend_Auth::getInstance();
 
-        if ($auth->hasIdentity()){
+        if ($auth->hasIdentity()) {
             $this->_redirect($this->getUrl());
         }
 
@@ -35,31 +47,25 @@ class AccountController extends Champs_Controller_MasterController{
         if (strlen($redirect) == 0)
             $redirect = $request->getServer('REQUEST_URI');
         if (strlen($redirect) == 0)
-            $redirect = $this->getUrl ();
+            $redirect = $this->getUrl();
 
-        $errors = null;
+        $errors = array();
 
-        if ($request->isPost()){
-            $errors = array();
+        if ($request->isPost()) {
+            
             $username = $request->getPost('username');
             $password = $request->getPost('password');
 
             if (strlen($username) == 0 || strlen($password) == 0)
                 $errors['username'] = 'Required field must not be blank';
 
-            if (count($errors) == 0){
+            if (count($errors) == 0) {
                 $adapter = new Champs_Auth_Doctrine($username, $password);
 
                 $result = $auth->authenticate($adapter);
 
-                if ($result->isValid()){
-                    $user = $adapter->getUser();
-
-                    $identity = $user;
-
-                    $auth->getStorage()->write($identity);
-
-                    $this->_redirect($redirect);
+                if ($result->isValid()) {
+                    $this->_redirect($this->getUrl());
                 }
 
                 $errors['username'] = 'Your login details were invalid';
@@ -75,13 +81,13 @@ class AccountController extends Champs_Controller_MasterController{
      *
      * show up the registration form
      */
-    public function registerAction(){
+    public function registerAction() {
         $request = $this->getRequest();
 
         $fp = new Champs_Form_Account_Register($this->em);
 
-        if ($request->isPost()){
-            if ($fp->process($request)){
+        if ($request->isPost()) {
+            if ($fp->process($request)) {
                 $session = new Zend_Session_Namespace('registration');
                 $session->user_id = $fp->user->id;
                 $this->_redirect($this->getUrl('complete'));
@@ -96,8 +102,8 @@ class AccountController extends Champs_Controller_MasterController{
      *
      * show up the message that if the user completed registration process
      */
-    public function completeAction(){
-        
+    public function completeAction() {
+
     }
 
     /**
@@ -105,11 +111,41 @@ class AccountController extends Champs_Controller_MasterController{
      *
      * show up the form for reset user's password
      */
-    public function resetpasswordAction(){
+    public function resetpasswordAction() {
 
     }
 
-    public function profileAction(){
+    public function profileAction() {
 
     }
+
+    /**
+     * Confirm and activate user account
+     */
+    public function confirmAction() {
+        if (Zend_Auth::getInstance()->hasIdentity())
+            $this->_redirect($this->getUrl());
+
+        $errors = array();
+
+        $action = $this->getRequest()->getParam('a');
+
+        switch ($action) {
+            case 'email':
+                $id = $this->getRequest()->getParam('id');
+                $key = $this->getRequest()->getParam('key');
+
+                $result = $this->em->getRepository('Champs\Entity\User')->activateAccount($id, $key);
+
+                if (!$result) {
+                    $errors['email'] = 'Error activating your account';
+                }
+
+                break;
+        }
+
+        $this->view->errors = $errors;
+        $this->view->action = $action;
+    }
+
 }
