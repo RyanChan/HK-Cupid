723de761f62aa0f7b721597f866f2c5ba468ea96
Ryan Chan <kimchan1314@gmail.com>
2012-12-29 17:48:51 +0800

    Added actions
    Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

--- application/modules/default/controllers/AlbumController.php
+++ application/modules/default/controllers/AlbumController.php
@@ -9,28 +9,67 @@ class AlbumController extends Champs_Controller_MasterController {
         parent::init();
     }
 
+    /**
+     * indexAction
+     *
+     * forward to brose action
+     */
     public function indexAction() {
         $this->_forward('browse');
     }
 
+    /**
+     * createAction
+     *
+     * create an album for user
+     */
     public function createAction() {
 
     }
 
+    /**
+     * editAction
+     *
+     * edit an album of user
+     */
     public function editAction() {
 
     }
 
+    /**
+     * deleteAction
+     *
+     * delete an album
+     */
     public function deleteAction() {
 
     }
 
+    /**
+     * browseAction
+     *
+     * browse all the albums
+     */
     public function browseAction() {
 
     }
 
+    /**
+     * photosAction
+     *
+     * browse specific album's photos
+     */
     public function photosAction() {
-        
+
+    }
+
+    /**
+     * albumsAction
+     *
+     * browse user's albums
+     */
+    public function albumsAction() {
+
     }
 
 }
