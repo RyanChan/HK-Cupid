9e2e1b7063e95321ee96997855aff08a2786eaa5
Ryan Chan <kimchan1314@gmail.com>
2013-01-03 19:19:20 +0800

    Added storeAlbumEntity method Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

--- library/Champs/Entity/Repository/AlbumRepository.php
+++ library/Champs/Entity/Repository/AlbumRepository.php
@@ -1,13 +1,35 @@
 <?php
+
 namespace Champs\Entity\Repository;
 
 use Champs\Entity\Album;
+//use Champs\Entity\User;
 use Doctrine\ORM\EntityRepository;
 
 /**
  * Album repository
  * @author RyanChan
  */
-class AlbumRepository extends EntityRepository{
+class AlbumRepository extends EntityRepository {
+
+    public function storeAlbumEntity(Album $album, $user_id) {
+        try {
+            // get the entity manager
+            $em = $this->getEntityManager();
+
+            // get the current user's entity
+            $user = $em->find('Champs\Entity\User', $user_id);
+
+            // assign user entity to album
+            $album->user = $user;
+
+            // persist & store album entity
+            $em->persist($album);
+            $em->flush();
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
 
 }
