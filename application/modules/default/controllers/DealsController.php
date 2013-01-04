d3fdc33bbbf7a37a43d9eb20e4ea22a20f17261f
Ryan Chan <kimchan1314@gmail.com>
2013-01-05 00:01:22 +0800

    Added deals controller Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

---
+++ application/modules/default/controllers/DealsController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Description of DealsController
+ *
+ * @author RyanChan <ryanchan.tc@gmail.com>
+ */
+class DealsController extends Champs_Controller_MasterController{
+
+    /**
+     * initialize controller
+     */
+    public function init(){
+        parent::init();
+    }
+
+    /**
+     * Index Action
+     */
+    public function indexAction() {
+
+    }
+}
