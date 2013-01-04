d1a2fc68b8c8c335161d4bf894e36db4064b5d4e
Ryan Chan <kimchan1314@gmail.com>
2013-01-05 00:05:54 +0800

    Implemented storePhotoEntity & findAllByAlbumEntity functions Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

--- library/Champs/Entity/Repository/PhotoRepository.php
+++ library/Champs/Entity/Repository/PhotoRepository.php
@@ -1,13 +1,52 @@
 <?php
+
 namespace Champs\Entity\Repository;
 
 use Champs\Entity\Photo;
+use Champs\Entity\User;
 use Doctrine\ORM\EntityRepository;
 
 /**
  * Photo repository
  * @author RyanChan
  */
-class PhotoRepository extends EntityRepository{
+class PhotoRepository extends EntityRepository {
+
+    /**
+     * Store photo entity to database
+     *
+     * @param \Champs\Entity\Photo $photo
+     */
+    public function storePhotoEntity(Photo $photo) {
+        try {
+            // get the entity manager
+            $em = $this->getEntityManager();
+            // persist the entity
+            $em->persist($photo);
+            // flush it
+            $em->flush();
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * Find all photos by album id
+     *
+     * @param type $album_id
+     */
+    public function findAllByAlbumEntity($album) {
+        try {
+            // get the entity manager
+            $em = $this->getEntityManager();
+            // create query
+            $query = $em->createQuery('SELECT p FROM Champs\Entity\Photo p, Champs\Entity\Album a WHERE p.album = ?1');
+            $query->setParameter(1, $album);
+            // return result
+            return $query->getResult();
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
 
 }
