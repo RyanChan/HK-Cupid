a8601517bdf88f13500c3b3c006ae6af01dfc3f8
Ryan Chan <kimchan1314@gmail.com>
2013-01-09 23:18:40 +0800

    Added fewer actions Signed-off-by:Ryan Chan <kimchan1314@gmail.com>

--- application/modules/default/controllers/DealsController.php
+++ application/modules/default/controllers/DealsController.php
@@ -8,6 +8,12 @@
 class DealsController extends Champs_Controller_MasterController{
 
     /**
+     *
+     * @var Champs\Entity\Repository\ProductRepository $productRepository
+     */
+    protected $productRepository = null;
+
+    /**
      * initialize controller
      */
     public function init(){
@@ -18,6 +24,85 @@ class DealsController extends Champs_Controller_MasterController{
      * Index Action
      */
     public function indexAction() {
-        
+        // get the request object
+        $request = $this->getRequest();
+        // get the view mode
+        $view = $request->getParam('view');
+
+        switch ($view) {
+            case 'newest':
+                break;
+            case 'hottest':
+                break;
+            case 'featured':
+                break;
+            default:
+                break;
+        }
+
+        $this->view->view = $view;
+    }
+
+    /**
+     * products Action
+     *
+     * User's products listing
+     */
+    public function productsAction() {
+
+    }
+
+    /**
+     * product Action
+     *
+     * single product details
+     */
+    public function productAction() {
+
+    }
+
+    /**
+     * create action
+     *
+     * create a product
+     */
+    public function createAction() {
+
+    }
+
+    /**
+     * edit Action
+     *
+     * edit a product
+     */
+    public function editAction() {
+
+    }
+
+    /**
+     * delete Action
+     *
+     * delete a product
+     */
+    public function deleteAction() {
+
+    }
+
+    /**
+     * payments Action
+     *
+     * list out all the payments of the current user
+     */
+    public function paymentsAction() {
+
+    }
+
+    /**
+     * payment Action
+     *
+     * show up the single payment
+     */
+    public function paymentAction () {
+
     }
 }
