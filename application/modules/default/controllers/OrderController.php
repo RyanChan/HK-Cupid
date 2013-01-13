4da0fb240c0a991c92b8b0ea4200b0eb5040e7d8
Ryan Chan <kimchan1314@gmail.com>
2013-01-13 13:34:12 +0800

    Added OrderController

---
+++ application/modules/default/controllers/OrderController.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Order controller
+ *
+ * @author RyanChan <ryanchan.tc@gmail.com>
+ */
+class OrderController extends Champs_Controller_MasterController implements Champs_Controller_Interface_CRUDInterface {
+
+    /**
+     * Create order action
+     */
+    public function createAction() {
+
+    }
+
+    /**
+     * delete order action
+     */
+    public function deleteAction() {
+
+    }
+
+    /**
+     * edit order action
+     */
+    public function editAction() {
+
+    }
+
+    /**
+     * order list action
+     */
+    public function indexAction() {
+
+    }
+
+}
